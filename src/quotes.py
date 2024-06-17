755f680281e9b31dea09ecb79b5822c5541c1b15
Mary Jonah <jonahmary17@gmail.com>
2024-06-17 07:43:52 +0000

    Feature: build initial version of quote of day

---
+++ src/quotes.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+import requests 
+
+app = Flask(__name__)
+
+API_URL = "https://zenquotes.io/api/today"
+
+@app.route("/")
+def index():
+    quote_of_day_data = requests.get(API_URL).json()
+    quote = quote_of_day_data[0]["q"]
+    return render_template("index.html", quote=quote)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ tests/test_random_quote.py
@@ -0,0 +1,17 @@
+import pytest
+from src.quotes import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as c:
+        yield c
+
+
+def test_index_status(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_index_content(client):
+    response = client.get("/")
+    assert response.data 
