cbd07b58d2b7554af4fee448fc0056fd41c1958d
Mary Jonah <jonahmary17@gmail.com>
2024-06-22 17:04:04 +0000

    Chore: include backend block in terraform file

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,3 +1,16 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+        }
+    }
+    
+    backend "s3" {
+        key = "terraform.tfstate"
+        region = "us-east-2"
+    }
+}
+
 provider "aws" {
     region = "us-east-2"
 }
