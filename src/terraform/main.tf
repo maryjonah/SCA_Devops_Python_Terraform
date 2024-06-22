e529d66d0ff38f7d917ef08218c7abf7232dc7e0
Mary Jonah <jonahmary17@gmail.com>
2024-06-22 16:12:40 +0000

    Chore: Update 8080 to port 80

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -39,31 +39,12 @@ resource "aws_instance" "flask" {
     }
 }
 
-data "aws_vpc" "default" {
- default = true
-}
-
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
-    vpc_id = data.aws_vpc.default.id
 
     ingress {
-        from_port   = 8080
-        to_port     = 8080
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port   = 5000
-        to_port     = 5000
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }    
-
-    ingress {
-        from_port   = 22
-        to_port     = 22
+        from_port   = 80
+        to_port     = 80
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
