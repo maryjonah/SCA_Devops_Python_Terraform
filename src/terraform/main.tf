7cbc7278496b5660dd72f69e4683fdf3c22e2685
Mary Jonah <jonahmary17@gmail.com>
2024-06-27 19:50:11 +0000

    Chore: Update ami to us-east-2's ubuntu ami

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -76,7 +76,7 @@ output "public_ip" {
 }
 
 resource "aws_instance" "flask" {
-    ami = "ami-011e54f70c1c91e17"
+    ami = "ami-0f30a9c3a48f3fa79"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.flask-terraform-sg.id]
 
