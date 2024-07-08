18477c7c8c6d7d4020a688e57de970506a536545
Mary Jonah <jonahmary17@gmail.com>
2024-07-08 22:30:40 +0000

    Chore: Include cpu monitor for host

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -121,3 +121,10 @@ resource "aws_instance" "flask" {
         Name = "sca-flask-terraform-assignment"
     }
 }
+
+resource "datadog_monitor" "cpumonitor" {
+  name = "cpu monitor"
+  type = "metric alert"
+  message = "CPU usage alert"
+  query = "avg(last_1m):avg:system.cpu.system{*} by {host} > 60"
+}
